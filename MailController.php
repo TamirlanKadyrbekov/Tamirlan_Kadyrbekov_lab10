037e0ea9fa551b9da5836c55753d243f876bf399
TamirlanKadyrbekov <79455936+TamirlanKadyrbekov@users.noreply.github.com>
2021-04-05 16:32:46 +0600

    Update MailController.php

--- MailController.php
+++ MailController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class MailController extends Controller
 {
-    //
+   public function send(){
+        $objDemo=new \stdClass();
+        $objDemo->demo_one="Demo One Value";
+        $objDemo->demo_two="Demo Two Value";
+        $objDemo->sender="Tamirlan.Kadyrbekov";
+        $objDemo->receiver="Tamirlan.Kadyrbekov";
+        Mail::to("190103045@stu.sdu.edu.kz")->send(new DemoEmail($objDemo));
+    }
 }
